32c68e9142ab4f64cb0914790e7ab1a260ba3409
Rishav Raj <rishav5raj3@example.com>
2026-06-15 18:08:04 +0530

    1st version

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+
+export default function page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="max-w-[60vw] font-mono text-amber-50 px-8 py-6">
+        <div>
+          <span>hey, i'm rishav raj</span> <br /> <br />
+          i enjoy designing, coding, and generally poking at problems until they make sense. <br /><br />
+          most of my time goes into solving problems for real people, not just shipping features. <br /> <br />
+          i’m happiest in fast-moving environments where the roadmap shifts and feedback is immediate. <br /><br />
+          early-stage is my zone: defining structure, testing assumptions, and iterating until the product actually clicks. <br /><br />
+        </div>
+
+        <div>
+          <a className="text-blue-900" target="_blank" href="https://github.com/rishavvrajj">github</a> {' '}
+          {' '} | {' '}
+          <a className="text-blue-900" target="_blank" href="https://www.linkedin.com/in/rishavv-rajj/">linkedin</a>
+          {' '} | {' '} 
+          <a className="text-blue-900" target="_blank" href="https://x.com/rishavvrajj">x</a> <br /> <br />
+        </div>
+              
+        <div>
+          yeah, that's it.
+        </div>
+      </div>
+    </div>
+  )
+}
