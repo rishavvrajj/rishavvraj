0dec3969ee613fe179d590695942f06f908de2cd
rishav raj <rishav5raj3@gmail.com>
2026-06-27 22:36:16 +0530

    fix: make portfolio responsive

--- src/components/fotter.tsx
+++ src/components/fotter.tsx
@@ -4,26 +4,28 @@ import { FaGithub, FaLinkedin, } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 
 const navlink =[
-  {label: 'github', href: "https://github.com/rishavvrajj", icon: <FaGithub size={25} />},
-  {label: 'linkedin', href: "https://www.linkedin.com/in/rishavv-rajj/", icon: <FaLinkedin size={25} />},
-  {label: 'twitter', href: "https://x.com/rishavvrajj", icon: <FaSquareXTwitter size={25} />},
+  {label: 'github', href: "https://github.com/rishavvrajj", icon: FaGithub},
+  {label: 'linkedin', href: "https://www.linkedin.com/in/rishavv-rajj/", icon: FaLinkedin},
+  {label: 'twitter', href: "https://x.com/rishavvrajj", icon: FaSquareXTwitter},
 ];
 
 export default function Fotter() {
   return (
-    <div>
+    <div className="text-blue-50">
       <div className="flex flex-col justify-center min-h-20 border-t-2 border-zinc-700">
-        <div className="flex justify-between items-center text-sm">
+        <div className="flex justify-between items-center text-[10px] md:text-xs lg:text-sm ">
           <div className="opacity-50"><span>rishav raj</span><br />Still learning, still building, still growing.</div>
           <div className="flex gap-5">
-            {navlink.map((item) => (
-              <Link target="_blank" key={item.label} href={item.href}>{item.icon}</Link>
-            ))}
+            {navlink.map((item) => {
+              const Icon = item.icon;
+              return (
+              <Link target="_blank" key={item.label} href={item.href}><Icon className="size-4 md:size-5 lg:size-6" /></Link>
+              )})}
           </div>
         </div>
       </div>
 
-      <div className="text-sm opacity-50 p-[20px_0px] border-t-2 border-zinc-700">
+      <div className="text-[10px] md:text-xs lg:text-sm opacity-50 p-[20px_0px] border-t-2 border-zinc-700">
         @ 2026 rishav raj
       </div>
     </div>
--- src/components/github.tsx
+++ src/components/github.tsx
@@ -1,22 +1,29 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
-import { GitHubCalendar } from "react-github-calendar"
+import { useEffect, useState } from "react";
+import { GitHubCalendar } from "react-github-calendar";
 
 export default function Github() {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setMounted(true)
+    setMounted(true);
   }, []);
 
-  if (!mounted) {
-    return null
-  };
+  if (!mounted) return null;
 
   return (
-    <div className="max-w-3xl flex flex-col items-center justify-center">
-        <GitHubCalendar blockSize={11.5} blockMargin={3} fontSize={12} username="rishavvrajj" />
+    <div className="text-blue-50 opacity-80 p-[10_0] flex flex-col items-center justify-center">
+      <div className="max-w-90 md:max-w-xl lg:max-w-3xl">
+        <GitHubCalendar
+          className="inline-block"
+          colorScheme="dark"
+          blockSize={11.5}
+          blockMargin={3}
+          fontSize={13}
+          username="rishavvrajj"
+        />
+      </div>
     </div>
   );
-};
+}
